964dafcd8639d3ad545fb053a17d90cba5fdaf0f
superman <1689851268@qq.com>
2023-03-13 21:30:44 +0800

    解决跨域问题

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
-import * as cors from 'cors';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,17 +9,15 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     // 处理跨域
-    app.use(
-        cors({
-            origin(origin, callback) {
-                if (origin === process.env.CROSS_DOMAIN_WHITELIST) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not allowed by CORS'));
-                }
-            },
-        }),
-    );
+    app.enableCors({
+        origin(origin, callback) {
+            if (origin === process.env.CROSS_DOMAIN_WHITELIST) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+    });
 
     // 全局注册异常过滤器
     app.useGlobalFilters(new HttpExceptionFilter());
