f8cf9d93a0efcb0138fac731f5e2c1b0bd9a563f
superman <1689851268@qq.com>
2023-03-13 15:03:24 +0800

    跨域配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalFilters(new HttpExceptionFilter()); // 注册异常过滤器
+
+    // 处理跨域
+    app.use(
+        cors({
+            origin(origin, callback) {
+                if (origin === process.env.CROSS_DOMAIN_WHITELIST) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not allowed by CORS'));
+                }
+            },
+        }),
+    );
+
+    // 全局注册异常过滤器
+    app.useGlobalFilters(new HttpExceptionFilter());
+
     await app.listen(3000);
 }
 bootstrap();
